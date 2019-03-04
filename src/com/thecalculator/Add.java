bc7237340ee46e5e5689c72c28c9e3a32e08b2be
RonjaBrandt <brandtronja@gmail.com>
2019-03-04 11:25:35 +0100

    Added interface "Divide"

--- src/com/thecalculator/Add.java
+++ src/com/thecalculator/Add.java
@@ -3,5 +3,4 @@ package com.thecalculator;
 public interface Add {
 
     long add(long number1, long number2);
-    long sum = 0;
 }
---
+++ src/com/thecalculator/Divide.java
@@ -0,0 +1,6 @@
+package com.thecalculator;
+
+public interface Divide {
+
+    long divide (long number1, long number2);
+}
