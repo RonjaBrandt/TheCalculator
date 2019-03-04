6a87a0932487dde455ec6cfc6d19f4a39fb1ef02
RonjaBrandt <brandtronja@gmail.com>
2019-03-04 11:14:31 +0100

    Updated method 'Add'
    "Made that the method 'Add' in 'Math' class can add two numbers"

--- src/com/thecalculator/Math.java
+++ src/com/thecalculator/Math.java
@@ -5,7 +5,8 @@ public class Math implements Add, Subtract {
 
     @Override
     public long add(long number1, long number2) {
-        return 0;
+        long sum = number1+number2;
+        return sum;
     }
 
     @Override
