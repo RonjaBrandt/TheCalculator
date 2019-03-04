8d9a6d1c4981ce75c1bea9e8a47494e1ca0a16ad
RonjaBrandt <brandtronja@gmail.com>
2019-03-04 11:12:21 +0100

    Updated MathTest.
    "Added test to see if Java can add and subtract."

--- test/com/thecalculator/MathTest.java
+++ test/com/thecalculator/MathTest.java
@@ -9,16 +9,23 @@ public class MathTest {
     @Test
     public void add() {
         Math math = new Math();
-
         assertEquals(0,math.add(1,1));
 
+
+        //See if Java can add
+        long sum = 10 + 11;
+        assertEquals(sum, 10 + 11);
 
     }
 
     @Test
     public void subtract() {
         Math math = new Math();
+        assertEquals(0, math.subtract(1, 1));
 
-        assertEquals(0,math.subtract(1,1));
+
+        //See if Java can subtract
+        long sum = 10-9;
+        assertEquals(sum, 10-9);
     }
 }
